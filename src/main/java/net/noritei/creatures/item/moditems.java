1e7fa90ec91a27ccf1b68614570e510ac77bfb5b
Nori <sachaskov@gmail.com>
2024-12-30 20:03:20 +0100

    Initial commit

---
+++ src/main/java/net/noritei/creatures/item/moditems.java
@@ -0,0 +1,29 @@
+package net.noritei.creatures.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+
+import net.noritei.creatures.creatures;
+
+// Register items in the mod
+public class moditems {
+    // Create a Deferred Register to hold items
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, creatures.modId);
+
+    // Register the item (Creeper Meat in this case)
+    public static final RegistryObject<Item> CREEPERMEAT = ITEMS.register("creeper_meat",
+            () -> new Item(new Item.Properties()
+                    .useItemDescriptionPrefix() // Ensures item description is prefixed
+                    .setId(ResourceKey.create(ForgeRegistries.ITEMS.getRegistryKey(), ResourceLocation.parse("creatures:creeper_meat")))));
+
+    // Register all items in the event bus
+    public static void register(IEventBus eventBus) {
+        // Register the Deferred Register to the event bus so that the items get registered
+        ITEMS.register(eventBus);
+    }
+}

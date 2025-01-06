578b480081480a5ce307d2dac1b289dc758e8593
Nori <sachaskov@gmail.com>
2025-01-06 14:42:15 +0100

    added recipes for creeper meat and block

--- build/tmp/.cache/expanded/zip_2adae0481bb7dd4d35ef44e965c8acbf/net/minecraft/world/item/crafting/RecipeAccess.java
+++
@@ -1,9 +0,0 @@
-package net.minecraft.world.item.crafting;
-
-import net.minecraft.resources.ResourceKey;
-
-public interface RecipeAccess {
-    RecipePropertySet propertySet(ResourceKey<RecipePropertySet> pPropertySet);
-
-    SelectableRecipe.SingleInputSet<StonecutterRecipe> stonecutterRecipes();
-}
